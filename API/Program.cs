3cad30314cde9e68205a40c48ee34967cafcf2dc
Pham-Hong-Ky <535arr@gmail.com>
2024-04-29 16:06:16 +0700

    chap 43

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,31 @@
 
+using API.Data;
 using API.Extensions;
+using API.Middleware;
+using Microsoft.EntityFrameworkCore;
 
 
 var builder = WebApplication.CreateBuilder(args);
+
+var host = builder.Build();
+
+using var scope = host.Services.CreateScope();
+
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    
+    await context.Database.MigrateAsync();
+    
+    await Seed.SeedUser(context);
+} 
+catch(Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured during migration");
+}
 
 // Add services to the container.
 builder.Services.AddControllers();
@@ -25,6 +48,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseRouting();
 
